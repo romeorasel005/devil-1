10cb9324870420be9d9ecc6119ced198c0e3e0ea
romeorasel005 <150465059+romeorasel005@users.noreply.github.com>
2024-03-15 20:35:14 +0600

    Update up.js

--- scripts/cmds/up.js
+++ scripts/cmds/up.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "up",
     aliases: ["up"],
     version: "1.0",
-    author: "Shinpei",
+    author: "Rômeo",
     role: 0,
     shortDescription: {
       en: "Displays the uptime of the bot."
@@ -17,16 +17,18 @@ module.exports = {
     }
   },
   onStart: async function ({ api, event, args }) {
+    
     const uptime = process.uptime();
     const seconds = Math.floor(uptime % 60);
     const minutes = Math.floor((uptime / 60) % 60);
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${hours} Hours ${minutes} Minutes ${seconds} Second`;
-    api.sendMessage(`Hello Master~ 👾
-    
-Your Bot Has Been Running For
+    const uptimeString = `${days} 𝙳𝚊𝚢𝚜 ${hours} 𝙷𝚘𝚞𝚛𝚜 ${minutes} 𝙼𝚒𝚗𝚞𝚝𝚎𝚜 ${seconds} 𝚂𝚎𝚌𝚘𝚗𝚍𝚜`;
+    api.sendMessage(`𝗛𝗲𝗹𝗹𝗼 𝗠𝗮𝘀𝘁𝗲𝗿~ 🐼,
 
-${uptimeString}.`, event.threadID);
+ 🫶 𝙔𝙤𝙪𝙧 𝙗𝙤𝙩 𝙞𝙨 𝙧𝙪𝙣𝙣𝙞𝙣𝙜 𝙛𝙧𝙤𝙢
+ 
+ 
+ ${uptimeString}.`, event.threadID);
   }
 };
